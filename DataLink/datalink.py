55287aff362e0cbd7b8aa813c2a986f18515b04e
AntonSukhov <RealComrade2011@yandex.ru>
2023-07-16 12:57:52 +0300

    Рефакторинг кода datalink.py и обновление файла DataLink.oxt

--- DataLink/datalink.py
+++ DataLink/datalink.py
@@ -12,13 +12,16 @@ def MessageBoxShow(captionText, messageText, messageBoxType = MESSAGEBOX):
 	return msgbox.execute()
 
 class DataLink(unohelper.Base, XJobExecutor):
+    implementationName = "vnd.datalink"                  #Имя реализации
+    serviceNames = ("com.sun.star.task.Job",)             #Список реализованных служб
+    
     def __init__(self, context):
         self.context = context
 
-    def trigger(self, args):
-        MessageBoxShow("Info", "Hello World!!!")
+    def trigger(self, args):                             #args - имя аргумента, определенное в файле Addons.xcu
+        MessageBoxShow("Info", "Hello World!!! Args: " + args)
 
 g_ImplementationHelper = unohelper.ImplementationHelper()
 
-g_ImplementationHelper.addImplementation(DataLink, 'vnd.datalink', ('com.sun.star.task.Job',),)
+g_ImplementationHelper.addImplementation(DataLink, DataLink.implementationName, DataLink.serviceNames,)
 
